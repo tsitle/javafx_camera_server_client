25f2f9e1f83576761e9f8bf740d8551f967114a8
tsitle <technisandk@gmail.com>
2025-03-05 17:45:37 +0100

    Add source code

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.ts.camera_streaming_client {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires kotlin.stdlib;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.ts.camera_streaming_client to javafx.fxml;
+    exports org.ts.camera_streaming_client;
+}
