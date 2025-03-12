da5fdfb6501fc758187da945303c2693a983e8b7
tsitle <technisandk@gmail.com>
2025-03-12 13:40:01 +0100

    ApiClientFncs: Don't unnecessarily fetch server status

--- src/main/kotlin/org/ts/pnp_camera_server_client/ApiClientFncs.kt
+++ src/main/kotlin/org/ts/pnp_camera_server_client/ApiClientFncs.kt
@@ -97,16 +97,10 @@ class ApiClientFncs(serverUrl: String = "", apiKey: String = "") {
 	 * @param doShow Show grid?
 	 */
 	fun setShowGrid(doShow: Boolean) {
-		val resultStat : Status = getServerStatus(false) ?: return
-
 		try {
-			if (resultStat.result != Status.Result.success) {
-				uiProps!!.statusMsg.set("Error reading status from server")
-			} else {
-				val resultPost : Status = apiInstance!!.procGridShow(if (doShow) 1 else 0)
-				if (resultPost.result != Status.Result.success) {
-					uiProps!!.statusMsg.set("Could not toggle ShowGrid")
-				}
+			val resultPost : Status = apiInstance!!.procGridShow(if (doShow) 1 else 0)
+			if (resultPost.result != Status.Result.success) {
+				uiProps!!.statusMsg.set("Could not toggle ShowGrid")
 			}
 		} catch (e: Exception) {
 			uiProps!!.statusMsg.set("Exception calling DefaultApi#procGridShow: ${e.message}")
@@ -120,47 +114,42 @@ class ApiClientFncs(serverUrl: String = "", apiKey: String = "") {
 	 * @param cam Camera(s) to activate
 	 */
 	fun setActiveCam(cam: DefaultApi.CamOutputCamEnable) {
-		val resultStat : Status = getServerStatus(false) ?: return
-
 		try {
-			if (resultStat.result != Status.Result.success) {
-				uiProps!!.statusMsg.set("Error reading status from server")
-			} else {
-				var doNothing = false
-				var doSwap = false
-				var doCamEnable : DefaultApi.CamOutputCamEnable? = null
-				var doCamDisable : DefaultApi.CamOutputCamDisable? = null
-				if ((cam == DefaultApi.CamOutputCamEnable.L && resultStat.outputCams == StatusCams.L) ||
-						(cam == DefaultApi.CamOutputCamEnable.R && resultStat.outputCams == StatusCams.R) ||
-						(cam == DefaultApi.CamOutputCamEnable.BOTH && resultStat.outputCams == StatusCams.BOTH)) {
-					doNothing = true
-				} else if ((cam == DefaultApi.CamOutputCamEnable.L && resultStat.outputCams == StatusCams.R) ||
-						(cam == DefaultApi.CamOutputCamEnable.R && resultStat.outputCams == StatusCams.L)) {
-					doSwap = true
-				} else if (cam == DefaultApi.CamOutputCamEnable.BOTH && resultStat.outputCams == StatusCams.L) {
-					doCamEnable = DefaultApi.CamOutputCamEnable.R
-				} else if (cam == DefaultApi.CamOutputCamEnable.BOTH && resultStat.outputCams == StatusCams.R) {
-					doCamEnable = DefaultApi.CamOutputCamEnable.L
-				} else if (cam == DefaultApi.CamOutputCamEnable.L && resultStat.outputCams == StatusCams.BOTH) {
-					doCamDisable = DefaultApi.CamOutputCamDisable.R
-				} else if (cam == DefaultApi.CamOutputCamEnable.R && resultStat.outputCams == StatusCams.BOTH) {
-					doCamDisable = DefaultApi.CamOutputCamDisable.L
+			var doNothing = false
+			var doSwap = false
+			var doCamEnable : DefaultApi.CamOutputCamEnable? = null
+			var doCamDisable : DefaultApi.CamOutputCamDisable? = null
+			val tmpOutputCams = uiProps!!.ctrlCamActive.value
+			if ((cam == DefaultApi.CamOutputCamEnable.L && tmpOutputCams == StatusCams.L.ordinal) ||
+					(cam == DefaultApi.CamOutputCamEnable.R && tmpOutputCams == StatusCams.R.ordinal) ||
+					(cam == DefaultApi.CamOutputCamEnable.BOTH && tmpOutputCams == StatusCams.BOTH.ordinal)) {
+				doNothing = true
+			} else if ((cam == DefaultApi.CamOutputCamEnable.L && tmpOutputCams == StatusCams.R.ordinal) ||
+					(cam == DefaultApi.CamOutputCamEnable.R && tmpOutputCams == StatusCams.L.ordinal)) {
+				doSwap = true
+			} else if (cam == DefaultApi.CamOutputCamEnable.BOTH && tmpOutputCams == StatusCams.L.ordinal) {
+				doCamEnable = DefaultApi.CamOutputCamEnable.R
+			} else if (cam == DefaultApi.CamOutputCamEnable.BOTH && tmpOutputCams == StatusCams.R.ordinal) {
+				doCamEnable = DefaultApi.CamOutputCamEnable.L
+			} else if (cam == DefaultApi.CamOutputCamEnable.L && tmpOutputCams == StatusCams.BOTH.ordinal) {
+				doCamDisable = DefaultApi.CamOutputCamDisable.R
+			} else if (cam == DefaultApi.CamOutputCamEnable.R && tmpOutputCams == StatusCams.BOTH.ordinal) {
+				doCamDisable = DefaultApi.CamOutputCamDisable.L
+			}
+			if (! doNothing && doCamEnable != null) {
+				val resultPost : Status = apiInstance!!.outputCamEnable(doCamEnable)
+				if (resultPost.result != Status.Result.success) {
+					uiProps!!.statusMsg.set("Could not enable camera $doCamEnable")
 				}
-				if (! doNothing && doCamEnable != null) {
-					val resultPost : Status = apiInstance!!.outputCamEnable(doCamEnable)
-					if (resultPost.result != Status.Result.success) {
-						uiProps!!.statusMsg.set("Could not enable camera $doCamEnable")
-					}
-				} else if (! doNothing && doCamDisable != null) {
-					val resultPost : Status = apiInstance!!.outputCamDisable(doCamDisable)
-					if (resultPost.result != Status.Result.success) {
-						uiProps!!.statusMsg.set("Could not deactivate camera $doCamDisable")
-					}
-				} else if (! doNothing && doSwap) {
-					val resultPost : Status = apiInstance!!.outputCamSwap()
-					if (resultPost.result != Status.Result.success) {
-						uiProps!!.statusMsg.set("Could not swap active camera")
-					}
+			} else if (! doNothing && doCamDisable != null) {
+				val resultPost : Status = apiInstance!!.outputCamDisable(doCamDisable)
+				if (resultPost.result != Status.Result.success) {
+					uiProps!!.statusMsg.set("Could not deactivate camera $doCamDisable")
+				}
+			} else if (! doNothing && doSwap) {
+				val resultPost : Status = apiInstance!!.outputCamSwap()
+				if (resultPost.result != Status.Result.success) {
+					uiProps!!.statusMsg.set("Could not swap active camera")
 				}
 			}
 		} catch (e: Exception) {
@@ -175,16 +164,10 @@ class ApiClientFncs(serverUrl: String = "", apiKey: String = "") {
 	 * @param valuePerc Zoom level in percent
 	 */
 	fun setZoom(valuePerc: Int) {
-		val resultStat : Status = getServerStatus(false) ?: return
-
 		try {
-			if (resultStat.result != Status.Result.success) {
-				uiProps!!.statusMsg.set("Error reading status from server")
-			} else {
-				val resultPost : Status = apiInstance!!.procRoiSize(valuePerc)
-				if (resultPost.result != Status.Result.success) {
-					uiProps!!.statusMsg.set("Could not set zoom level")
-				}
+			val resultPost : Status = apiInstance!!.procRoiSize(valuePerc)
+			if (resultPost.result != Status.Result.success) {
+				uiProps!!.statusMsg.set("Could not set zoom level")
 			}
 		} catch (e: Exception) {
 			uiProps!!.statusMsg.set("Exception calling DefaultApi#procRoiSize: ${e.message}")
@@ -198,16 +181,10 @@ class ApiClientFncs(serverUrl: String = "", apiKey: String = "") {
 	 * @param valuePerc Brightness in percent
 	 */
 	fun setBrightness(valuePerc: Int) {
-		val resultStat : Status = getServerStatus(false) ?: return
-
 		try {
-			if (resultStat.result != Status.Result.success) {
-				uiProps!!.statusMsg.set("Error reading status from server")
-			} else {
-				val resultPost : Status = apiInstance!!.procBncBrightness(valuePerc)
-				if (resultPost.result != Status.Result.success) {
-					uiProps!!.statusMsg.set("Could not set brightness")
-				}
+			val resultPost : Status = apiInstance!!.procBncBrightness(valuePerc)
+			if (resultPost.result != Status.Result.success) {
+				uiProps!!.statusMsg.set("Could not set brightness")
 			}
 		} catch (e: Exception) {
 			uiProps!!.statusMsg.set("Exception calling DefaultApi#procBncBrightness: ${e.message}")
