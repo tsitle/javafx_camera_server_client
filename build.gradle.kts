cd57d8ad849d0da674e312f5e8c4d21d185b6b55
tsitle <technisandk@gmail.com>
2025-03-21 20:01:27 +0100

    build.gradle.kts: Mainly improve runtime/jpackage support

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,9 @@ plugins {
 	id("org.jetbrains.kotlin.jvm") version "2.1.10"
 	id("org.openjfx.javafxplugin") version "0.1.0"  // see https://github.com/openjfx/javafx-gradle-plugin
 	id("org.beryx.runtime") version "1.13.1"  // see https://badass-runtime-plugin.beryx.org/releases/latest/
+	id("com.google.osdetector") version "1.7.3"  // see https://github.com/google/osdetector-gradle-plugin
 	//id("org.barfuin.gradle.taskinfo") version "2.1.0"  // show Gradle Task Graph by '$ ./gradlew tiTree <TASK_NAME>'
 }
-
-group = "org.ts"
-version = "${project.findProperty("appVersion")}"  // read 'appVersion' from 'gradle.properties'
-
-println("Client app version: $version")
 
 repositories {
 	mavenCentral()
@@ -20,23 +16,59 @@ repositories {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-val osName: String = if (OperatingSystem.current().isMacOsX) {
-		"macos"
-	} else if (OperatingSystem.current().isLinux) {
-		"linux"
-	} else if (OperatingSystem.current().isWindows) {
-		"win"
-	} else {
-		throw Error("Operating System not supported")
+group = "org.ts"
+version = "${project.findProperty("appVersion")}"  // read 'appVersion' from 'gradle.properties'
+
+println("Client app version: $version")
+
+// get value of property 'rootProject' from 'settings.gradle' file
+val tmpPropProj = project.findProperty("rootProject")!! as org.gradle.api.internal.project.DefaultProject
+val tmpProjName = tmpPropProj.project!!.name  // tmpPropProj.project is actually nullable (Gradle 8.10)
+// read 'appImageName' from 'gradle.properties'
+val tmpAppImageName = project.findProperty("appImageName")!! as String
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+fun getOperatingSystemName(): String {
+	return if (OperatingSystem.current().isMacOsX) {
+			"macos"
+		} else if (OperatingSystem.current().isLinux) {
+			"linux"
+		} else if (OperatingSystem.current().isWindows) {
+			"win"
+		} else {
+			throw Error("Operating System not supported")
+		}
+}
+
+fun getCpuArchitecture(): String {
+	return when (System.getProperty("os.arch")) {
+			"x86_64", "x64", "amd64" -> "x64"
+			"aarch64" -> "aarch64"
+			else -> throw Error("CPU Architecture not supported")
+		}
+}
+
+fun getLinuxDistroType(): String {
+	if (getOperatingSystemName() != "linux") {
+		return "none"
 	}
-val cpuArch: String = when (System.getProperty("os.arch")) {
-		"x86_64", "amd64" -> "x64"
-		"aarch64" -> "aarch64"
-		else -> throw Error("CPU Architecture not supported")
+	val tmpRel = osdetector.release
+	if (tmpRel.isLike("debian")) {
+		return "debian"
 	}
+	if (tmpRel.isLike("redhat") || tmpRel.isLike("fedora")) {
+		return "redhat"
+	}
+	throw Error("Linux distribution type '${tmpRel}' not supported")
+}
+
+val osName: String = getOperatingSystemName()
+val cpuArch: String = getCpuArchitecture()
 if (osName == "win" && cpuArch != "x64") {
 	throw Error("Cannot build for ${osName}-${cpuArch}")
 }
+val lxDistroType: String = getLinuxDistroType()
 
 println("Host: ${osName}-${cpuArch}")
 
@@ -57,11 +89,11 @@ java {
 	 *   $ ./gradlew -q javaToolchains
 	 */
 
-	// select e JDK version that is >= as the one that was used to build the OpenCV library with
+	// for OpenJFX version 23 the minimum JDK version is 21
 	/*sourceCompatibility = JavaVersion.VERSION_21*/
 	toolchain {
 		languageVersion.set(JavaLanguageVersion.of(21))
-		vendor.set(JvmVendorSpec.BELLSOFT)  // we need a JDK that includes the JavaFX SDK
+		vendor.set(JvmVendorSpec.BELLSOFT)  // we need a JDK that includes the JavaFX SDK - like Bellsoft's 'Full' Liberica JDK
 	}
 }
 
@@ -70,7 +102,7 @@ kotlin {
 }
 
 javafx {
-	version = "23.0.2"  // for OpenJFX 23 the minimum JDK is 21
+	version = "23.0.2"  // for OpenJFX version 23 the minimum JDK version is 21
 	modules("javafx.controls", "javafx.fxml", "javafx.swing")
 }
 
@@ -89,11 +121,7 @@ dependencies {
 
 distributions {
 	main {
-		// get value of property 'rootProject' from 'settings.gradle' file
-		val tmpPropProj = project.findProperty("rootProject")!! as org.gradle.api.internal.project.DefaultProject
-		val tmpProjName = tmpPropProj.project!!.name  // tmpPropProj.project is actually nullable (Gradle 8.10)
-
-		distributionBaseName = "${tmpProjName}-${osName}-${cpuArch}"
+		distributionBaseName = "${tmpProjName}-${osName}"
 
 		contents {
 			if (osName == "win") {
@@ -108,22 +136,20 @@ distributions {
 // org.beryx.runtime: Creates an image containing the application, a custom JRE, and appropriate start scripts
 runtime {
 	// some default options
-	addOptions("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages")
+	addOptions("--strip-debug", "--compress", "zip-6", "--no-header-files", "--no-man-pages")
 	// list of required modules (instead of using modules-info.java)
 	modules.set(listOf(
-		"javafx.base",
-		"javafx.controls",
-		"javafx.fxml",
-		"javafx.swing",
-		"java.logging"  // for java.util.logging
-	))
+			"javafx.base",
+			"javafx.controls",
+			"javafx.fxml",
+			"javafx.swing",
+			"java.logging"  // for java.util.logging
+		))
+	imageDir.set(File(layout.buildDirectory.get().toString(), "${tmpProjName}-${osName}-${cpuArch}-${version}-launcher-w_jre"))  // default is 'image/'
+	imageZip.set(File(layout.buildDirectory.get().toString(), "${tmpProjName}-${osName}-${cpuArch}-${version}-launcher-w_jre.zip"))  // default is 'image.zip'
 
 	jpackage {
-		val tmpAppImageName = project.findProperty("appImageName")!! as String  // read 'appImageName' from 'gradle.properties'
-
 		installerOptions = listOf(
-				"--name",
-				"${tmpAppImageName}-installer",  // installer name (the version number will be appended automatically)
 				"--description",
 				project.description,  // 'description' from 'gradle.properties'
 				"--copyright",
@@ -131,16 +157,37 @@ runtime {
 				"--vendor",
 				project.findProperty("appVendor")!! as String  // read 'appVendor' from 'gradle.properties'
 			)
-		imageName = tmpAppImageName
+		imageName = when (osName) {
+				"macos" -> {
+					// on macOS results in e.g. 'JavaFX Camera Server Client-x64-1.2.app'
+					"${tmpAppImageName}-${cpuArch}-${version}"
+				}
+				"linux" -> {
+					// on Linux results in e.g. 'javafx_camera_server_client-linux-x64-1.2'
+					//"${tmpProjName}-${osName}-${cpuArch}-${version}"
+					// on Linux results in e.g. 'javafx_camera_server_client'
+					tmpProjName
+				}
+				else -> {
+					// on Win results in e.g. 'JavaFX Camera Server Client'
+					tmpAppImageName
+				}
+			}
+		installerName = when (osName) {
+				"macos" -> "${tmpProjName}-${cpuArch}"  // version will be autom. appended - on macOS results in e.g. 'javafx_camera_server_client-x64-1.2.pkg'
+				"linux" -> tmpProjName  // version and cpuArch will be autom. appended - on Linux results in e.g. 'javafx_camera_server_client-1.2-1.x86_64.rpm'
+				else -> "${tmpProjName}-${cpuArch}-installer"  // version will be autom. appended - on Win results in e.g. 'javafx_camera_server_client-x64-installer-1.2.msi'
+			}
+		outputDir = "${tmpProjName}-${osName}-${cpuArch}-${version}-pack-w_jre"  // default is 'jpackage/'
 
 		// set installerType (can be set from command line: e.g. './gradlew jpackage -PinstallerType=msi')
 		installerType = project.findProperty("installerType") as String? ?:
 				when (osName) {
-					"macos" -> "pkg"  // dmg
-					"linux" -> "deb"  // rpm
-					else -> "exe"  // msi
+					"macos" -> "pkg"  // or dmg
+					"linux" -> { if (lxDistroType == "debian") "deb" else "rpm" }
+					else -> "exe"  // or msi
 				}
-		println("installerType=$installerType")
+		println("InstallerType: $installerType")
 
 		//
 		///
@@ -148,31 +195,36 @@ runtime {
 		val tmpInstOptions: MutableList<String> = mutableListOf()
 		///
 		if (osName == "macos") {
-			tmpImageOptions.addAll(listOf("--mac-package-identifier", tmpAppImageName))
+			tmpImageOptions.addAll(listOf("--mac-package-identifier", project.findProperty("macosPackageId")!! as String))
 		}
 		///
-		/*
 		when (installerType) {
-			"pkg" -> {
+			"dmg", "pkg" -> {
+				/*
 				tmpImageOptions.addAll(listOf("--icon", "src/main/resources/icon.icns"))
 				tmpInstOptions.addAll(listOf("--license-file", "package/LICENSE-OS-Installer.txt"))
-			}
-			"dmg" -> {
+				*/
 			}
 			"deb", "rpm" -> {
+				/*
 				tmpImageOptions.addAll(listOf("--icon", "src/main/resources/icon_256x256.png"))
+				*/
 				tmpInstOptions.addAll(listOf(
 						"--linux-menu-group", "Utility",
 						"--linux-shortcut"
 					))
+				/*
 				if (installerType == "deb") {
 					tmpInstOptions.addAll(listOf("--linux-deb-maintainer", "nobody@nowhere.org"))
 				} else {
 					tmpInstOptions.addAll(listOf("--linux-rpm-license-type", "GPLv3"))
 				}
+				*/
 			}
 			"exe" -> {
+				/*
 				tmpImageOptions.addAll(listOf("--icon", "src/main/resources/icon.ico"))
+				*/
 				tmpInstOptions.addAll(listOf(
 						// "--win-per-user-install",  // install only for current user
 						// "--win-console",  // shows what Java outputs to the console
@@ -180,10 +232,9 @@ runtime {
 						"--win-menu", "--win-shortcut"
 					))
 			}
-			else -> {  // msi
-			}
+			"msi" -> {}
+			else -> throw Error("Invalid installerType: '$installerType'")
 		}
-		*/
 		///
 		imageOptions = tmpImageOptions
 		installerOptions = tmpInstOptions
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,10 @@
-plugins {
+/*
+ * Since we cannot use Gluon's GraalVM or Bellsoft's 'Full' Liberica JDK (which includes JavaFX)
+ * the foojay-resolver plugin doesn't help us at all
+ */
+/*plugins {
 	// automatically configure (and download) JVM according to toolchain config in build.gradle.kts
 	id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"  // see https://plugins.gradle.org/plugin/org.gradle.toolchains.foojay-resolver-convention
-}
+}*/
 
 rootProject.name = "javafx_camera_server_client"
