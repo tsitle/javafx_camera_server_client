67f0cd5711105f91a6a58c266cbf6650870dc33b
tsitle <technisandk@gmail.com>
2025-03-21 19:58:50 +0100

    launch_wrappers: Add support for jlink launchers

--- src/launch_wrappers/launcher-linux.sh
+++ src/launch_wrappers/launcher-linux.sh
@@ -3,11 +3,13 @@
 #
 # Launcher for the Camera Server Client app.
 #
-# Requires that either env var JAVA_HOME is set or '$ command -v java' can find a Java executable (JDK or JRE).
-# The env var JAVA_HOME can be set down below.
+# If not bundled with a JRE then it
+#   requires that either env var JAVA_HOME is set or '$ command -v java' can find a Java executable (JDK or JRE).
+#   The env var JAVA_HOME can be set down below.
 #
-# Show list of available JDKs under Linux and change default if necessary:
-#   $ sudo update-alternatives --config java
+#   Show list of available JDKs under Linux and change default if necessary:
+#     $ sudo update-alternatives --config java
+# If bundled with a JRE no further requirements exist.
 #
 # by TS, Mar 2025
 #
@@ -46,49 +48,51 @@ LVAR_ARCH="$(_getCpuArch)"
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-# examples for JAVA_HOME under Linux:
-#export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-21.0.6.0.7-1.el9.x86_64
-#export JAVA_HOME=/usr/lib/jvm/java-23-openjdk-23.0.2.0.7-1.rolling.el9.x86_64
-#export JAVA_HOME=/opt/liberica-jdk-21.0.6-full-linux_x64
-#export JAVA_HOME=/opt/graalvm-jdk-22.0.2-linux_x64
+if [ ! -f "$(dirname "$0")/bin/java" ]; then
+	# examples for JAVA_HOME under Linux:
+	#export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-21.0.6.0.7-1.el9.x86_64
+	#export JAVA_HOME=/usr/lib/jvm/java-23-openjdk-23.0.2.0.7-1.rolling.el9.x86_64
+	#export JAVA_HOME=/opt/liberica-jdk-21.0.6-full-linux_x64
+	#export JAVA_HOME=/opt/graalvm-jdk-22.0.2-linux_x64
 
-# ----------------------------------------------------------------------------------------------------------------------
+	# ------------------------------------------------------------------------------------------------------------------
 
-TMP_JAVA_PATH=""
-if [ -n "${JAVA_HOME}" ]; then
-	TMP_JAVA_PATH="${JAVA_HOME}/bin/"
-	if [ ! -x "${TMP_JAVA_PATH}java" ]; then
-		echo "Error: Could not find Java executable in JAVA_HOME ('${JAVA_HOME}')" >>/dev/stderr
+	TMP_JAVA_PATH=""
+	if [ -n "${JAVA_HOME}" ]; then
+		TMP_JAVA_PATH="${JAVA_HOME}/bin/"
+		if [ ! -x "${TMP_JAVA_PATH}java" ]; then
+			echo "Error: Could not find Java executable in JAVA_HOME ('${JAVA_HOME}')" >>/dev/stderr
+			exit 1
+		fi
+	else
+		command -v java >/dev/null 2>&1 || {
+			echo "Error: Could not find Java executable" >>/dev/stderr
+			exit 1
+		}
+	fi
+	TMP_JAVA_VERSION="$("${TMP_JAVA_PATH}java" --version | head -n1 | cut -f2 -d\  | cut -f1 -d.)"
+	if [ -z "${TMP_JAVA_VERSION}" ]; then
+		echo "Error: Could not check Java version" >>/dev/stderr
 		exit 1
 	fi
-else
-	command -v java >/dev/null 2>&1 || {
-		echo "Error: Could not find Java executable" >>/dev/stderr
+	# shellcheck disable=SC2086
+	if [ ${TMP_JAVA_VERSION} -lt ${LCFG_JAVA_MIN_LANG_LEVEL} ]; then
+		{
+			echo "Error: Java version needs to be >= ${LCFG_JAVA_MIN_LANG_LEVEL} (is ${TMP_JAVA_VERSION})"
+			echo -e "\nUse JAVA_HOME in this script to point '\$ java' to a valid JDK/JRE"
+			echo "or use something like"
+			if [ "${LCFG_OS_TYPE}" = "macos" ]; then
+				echo "  $ JAVA_HOME=\"\$(/usr/libexec/java_home -v ${LCFG_JAVA_MIN_LANG_LEVEL})\" ./launcher-${LCFG_OS_TYPE}.sh"
+			else
+				echo "  $ sudo update-alternatives --config java"
+			fi
+		} >>/dev/stderr
 		exit 1
-	}
-fi
-TMP_JAVA_VERSION="$("${TMP_JAVA_PATH}java" --version | head -n1 | cut -f2 -d\  | cut -f1 -d.)"
-if [ -z "${TMP_JAVA_VERSION}" ]; then
-	echo "Error: Could not check Java version" >>/dev/stderr
-	exit 1
-fi
-# shellcheck disable=SC2086
-if [ ${TMP_JAVA_VERSION} -lt ${LCFG_JAVA_MIN_LANG_LEVEL} ]; then
-	{
-		echo "Error: Java version needs to be >= ${LCFG_JAVA_MIN_LANG_LEVEL} (is ${TMP_JAVA_VERSION})"
-		echo -e "\nUse JAVA_HOME in this script to point '\$ java' to a valid JDK/JRE"
-		echo "or use something like"
-		if [ "${LCFG_OS_TYPE}" = "macos" ]; then
-			echo "  $ JAVA_HOME=\"\$(/usr/libexec/java_home -v ${LCFG_JAVA_MIN_LANG_LEVEL})\" ./launcher-${LCFG_OS_TYPE}.sh"
-		else
-			echo "  $ sudo update-alternatives --config java"
-		fi
-	} >>/dev/stderr
-	exit 1
-fi
+	fi
 
-# ----------------------------------------------------------------------------------------------------------------------
+	# ------------------------------------------------------------------------------------------------------------------
 
-export JAVA_OPTS="--module-path lib --add-modules=javafx.controls,javafx.fxml"
+	export JAVA_OPTS="--module-path lib --add-modules=javafx.controls,javafx.fxml"
+fi
 
 ./bin/javafx_camera_server_client
--- src/launch_wrappers/launcher-win.cmd
+++ src/launch_wrappers/launcher-win.cmd
@@ -22,13 +22,37 @@ title %0
 
 @REM -------------------------------------------------------------------------------------------------------------------
 
+@REM If this script part of a JLink Launcher release we don't need to set the JAVA_OPTS
+set TMPDIRNAME=%~dp0
+if exist %TMPDIRNAME%\bin\java.exe goto allReady
+
 @REM examples for JAVA_HOME under Windows:
 @REM set JAVA_HOME=C:\Program Files\Java\openjdk-22.0.2-win_x64
 
+if not "%JAVA_HOME%" == "" goto haveJavaHome
+echo.
+echo Error: 'JAVA_HOME' is not set.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+exit /b 1
+
+:haveJavaHome
+
+if exist %JAVA_HOME%\bin\java.exe goto haveJavaExe
+echo.
+echo Error: Cannot find '%JAVA_HOME%\bin\java.exe'.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+exit /b 1
+
 @REM -------------------------------------------------------------------------------------------------------------------
 
-set LVAR_ARCH=x64
+:haveJavaExe
 
 set JAVA_OPTS=--module-path lib --add-modules=javafx.controls,javafx.fxml
+
+:allReady
 
 call bin\javafx_camera_server_client.bat
